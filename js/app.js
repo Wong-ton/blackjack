c7bd71809f2ab9cafa8a849ddeeddc320bb6323a
Jason Wong <jwong175@hotmail.com>
2019-07-10 16:05:39 -0700

    Fixed Ace score problems, win condition statements, and rendered cards

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ let dealerSum = 0;
 let pNumOfWins = 0;
 let dNumOfWins = 0;
 let turnOver = false;
+let hasAce = false;
 
 
 
@@ -20,9 +21,9 @@ function createDeck() {
             if (cardValue[x] === "J" || cardValue[x] === "Q" || cardValue[x] === "K")
                 points = 10;
             if (cardValue[x] === "A")
-                points = 11;            // MAY HAVE TO ADJUST WEIGHT HERE TO 1 WHEN SUM IS > 21
+                points = 11;
             
-                deck.push({Value: cardValue[x], Suit: suits[i], Points: points})
+                deck.push({value: cardValue[x], suit: suits[i], points: points})
         }
     }
     return deck;
@@ -47,8 +48,25 @@ function shuffle(deck) {
     return deck;
   }
 
+function renderHand() {
+    for (let i = 0; i < playerHand.length; i++) {
+        const suit = playerHand[i].suit
+        const value = isNaN(playerHand[i].value) ? playerHand[i].value : playerHand[i].value === "10" ? "r" + playerHand[i].value : "r0" + playerHand[i].value
+        const $card = $('<div/>');
+        $card.addClass(`card ${suit} ${value}`);
+        $card.appendTo('#playerHand');
+    }
+    for (let i = 0; i < dealerHand.length; i++) {
+        const suit = dealerHand[i].suit
+        const value = isNaN(dealerHand[i].value) ? dealerHand[i].value : dealerHand[i].value === "10" ? "r" + dealerHand[i].value : "r0" + dealerHand[i].value
+        const $card = $('<div/>');
+        $card.addClass(`card ${suit} ${value}`);
+        $card.appendTo('#dealerHand');
+    }
+}
 
 function deal() {
+    
     let pCard1 = deck.pop();
     playerHand.push(pCard1);
     let dCard1 = deck.pop();
@@ -57,8 +75,11 @@ function deal() {
     playerHand.push(pCard2);
     let dCard2 = deck.pop();
     dealerHand.push(dCard2);
+    renderHand();
     getScore(playerHand);
     getScore(dealerHand);
+    updateScores();
+    check21();
     // Update points & Check sum for winner (maybe 2 functions)
 }
 
@@ -66,15 +87,15 @@ function deal() {
 // Calculates the score of the player/dealer hands and adjusts ACE value depending on of it's over 21 or not.
 function getScore(hand){
     let score = 0;
-    let hasAce = false;
+    // let hasAce = false;
     for (let i = 0; i < hand.length; i++) {
-      score += hand[i].Points
-      if (hand[i].Points == 11) {
-        hasAce = true;
-      } if (hasAce && score > 21) {
-        score -= 10;
-        hasAce = false;
-      }
+        score += hand[i].points
+        if (hand[i].points == 11) {
+            hasAce = true;
+        } if (hasAce && score > 21) {
+            score -= hand.filter(e => e.points === 11).length === 2 && hand.length > 2 ? 20 : hand.filter(e => e.points === 11).length === 3 && hand.length > 2 ? 30 : 10
+            hasAce = false;
+        }
     }
     return score; 
 }
@@ -86,22 +107,52 @@ function updateScores() {
 }
 
 function checkWinner() {
-    updateScores();
     if (turnOver) {
-        while (dealerSum < 17 && dealerSum < playerSum) {
+        while (dealerSum < 17) {
             hit(dealerHand);
             updateScores();
         }
-    } // If player and bust
-    if (playerSum > dealerSum && playerSum <= 21 || dealerSum > 21) {
-        pNumOfWins += 1;
-        console.log('You WIN!')
-    } else if (playerSum < dealerSum && dealerSum <= 21 || playerSum > 21) {
-    dNumOfWins += 1;
-    console.log('You LOSE!')
-    } else if (playerSum == dealerSum) {
-        console.log('PUSH!')
     }
+    if (playerSum === dealerSum) {
+        return console.log('You and the dealer have the same score, so it\'s a PUSH.');
+    }
+    else if (playerSum > dealerSum && playerSum < 22) {
+        pWin();
+        return console.log('Your hand beats the dealer\'s. You WIN!');
+    } else if (playerSum < dealerSum && dealerSum < 22) {
+        dWin();
+        return console.log('Dealer\'s hand beats yours. You LOSE!');
+    }
+}
+
+function checkBust() {
+     if (playerSum > 21) {
+        dWin();
+        console.log('You busted! You LOSE!')
+    } else if (dealerSum > 21) {
+        pWin();
+        return console.log('Dealer busted! You WIN!');
+    } 
+}
+
+function check21() {
+    if (playerSum === 21 && playerSum > dealerSum) {
+        pWin();
+        console.log('BLACKJACK! You WIN!');
+    } else if (dealerSum === 21 && dealerSum > playerSum) {
+        dWin();
+        console.log('BLACKJACK! You LOSE!');
+    } else if (playerSum && dealerSum === 21) {
+        console.log('Wow, double blackjack. PUSH!');
+    }
+}
+
+function pWin() {
+    pNumOfWins += 1;
+}
+
+function dWin() {
+    dNumOfWins += 1;
 }
 
 // Add next card to the player/dealer's hand, calculate score, and check if there's a winner.
@@ -109,17 +160,17 @@ function hit(hand) {
     let nextCard = deck.pop();
     hand.push(nextCard);
     getScore(hand);
-    checkWinner();
+    updateScores();
+    checkBust();
     console.log(nextCard, 'Score : ', getScore(hand));
     // Update points & Check sum for winner (maybe 2 functions)
 }
 
-function stay() {
+function stand() {
     turnOver = true;
+    checkBust();
     checkWinner();
-
-
-    // Turn over, move onto next player
+// Turn over, move onto next player
 }
 
 function reset() {
@@ -129,32 +180,29 @@ function reset() {
     let playerSum = 0;
     let dealerSum = 0;
     turnOver = false;
+    hasAce = false;
 }
 
 
-$('#play-button').on('click', () => {
+$('#deal-button').on('click', () => {
     reset();
     createDeck();
     shuffle(deck);
     deal();
+    updateScores();
     console.log('Game started');
-    console.log('Player Hand', playerHand);
-    console.log('Dealer Hand', dealerHand);
+    console.log(deck);
+    console.log('Player Hand', playerHand, 'Points : ', playerSum);
+    console.log('Dealer Hand', dealerHand, 'Points : ', dealerSum);
+    $('#deal-button').css({'visibility':'hidden'});
+    $('#hit-button').css({'visibility':'visible'});
+    $('#stand-button').css({'visibility':'visible'});
 })
 
 $('#hit-button').on('click', function() {
     hit(playerHand);
-    checkWinner();
 })
 
-$('#stay-button').on('click', function() {
-    stay();
+$('#stand-button').on('click', function() {
+    stand();
 })
-
-
-createDeck();
-shuffle(deck);
-deal();
-console.log(deck);
-console.log('Player Hand', playerHand, getScore(playerHand));
-console.log('Dealer Hand', dealerHand, getScore(dealerHand));
