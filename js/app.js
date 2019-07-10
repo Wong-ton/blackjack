6929c47c8447a8244cf773a05277434b5ae5be80
Jason Wong <jwong175@hotmail.com>
2019-07-10 09:23:32 -0700

    Added actual functionality to some functions, tweeked some other functions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 let deck = [];
 const suits = ["spades", "hearts", "clubs", "diamonds"]
 const cardValue = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-const playerHand = [];
-const dealerHand = [];
+let playerHand = [];
+let dealerHand = [];
 let playerSum = 0;
 let dealerSum = 0;
 let pNumOfWins = 0;
 let dNumOfWins = 0;
+let turnOver = false;
 
 
 
@@ -55,7 +56,9 @@ function deal() {
     let pCard2 = deck.pop();
     playerHand.push(pCard2);
     let dCard2 = deck.pop();
-    dealerHand.push(dCard2)
+    dealerHand.push(dCard2);
+    getScore(playerHand);
+    getScore(dealerHand);
     // Update points & Check sum for winner (maybe 2 functions)
 }
 
@@ -65,50 +68,93 @@ function getScore(hand){
     let score = 0;
     let hasAce = false;
     for (let i = 0; i < hand.length; i++) {
-    //   let card = hand[i];
       score += hand[i].Points
       if (hand[i].Points == 11) {
         hasAce = true;
       } if (hasAce && score > 21) {
-        return score - 10;
+        score -= 10;
+        hasAce = false;
       }
     }
-     return score; 
-  }
+    return score; 
+}
 
 
+function updateScores() {
+    dealerSum = getScore(dealerHand);
+    playerSum = getScore(playerHand);
+}
+
+function checkWinner() {
+    updateScores();
+    if (turnOver) {
+        while (dealerSum < 17 && dealerSum < playerSum) {
+            hit(dealerHand);
+            updateScores();
+        }
+    } // If player and bust
+    if (playerSum > dealerSum && playerSum <= 21 || dealerSum > 21) {
+        pNumOfWins += 1;
+        console.log('You WIN!')
+    } else if (playerSum < dealerSum && dealerSum <= 21 || playerSum > 21) {
+    dNumOfWins += 1;
+    console.log('You LOSE!')
+    } else if (playerSum == dealerSum) {
+        console.log('PUSH!')
+    }
+}
+
+// Add next card to the player/dealer's hand, calculate score, and check if there's a winner.
 function hit(hand) {
-    let card1 = deck.pop();
-    hand.push(card1); 
+    let nextCard = deck.pop();
+    hand.push(nextCard);
+    getScore(hand);
+    checkWinner();
+    console.log(nextCard, 'Score : ', getScore(hand));
     // Update points & Check sum for winner (maybe 2 functions)
 }
 
 function stay() {
+    turnOver = true;
+    checkWinner();
+
+
     // Turn over, move onto next player
 }
 
 function reset() {
     let deck = [];
-    // & Set other game variables back to original state
+    let playerHand = [];
+    let dealerHand = [];
+    let playerSum = 0;
+    let dealerSum = 0;
+    turnOver = false;
 }
+
+
+$('#play-button').on('click', () => {
+    reset();
+    createDeck();
+    shuffle(deck);
+    deal();
+    console.log('Game started');
+    console.log('Player Hand', playerHand);
+    console.log('Dealer Hand', dealerHand);
+})
+
+$('#hit-button').on('click', function() {
+    hit(playerHand);
+    checkWinner();
+})
+
+$('#stay-button').on('click', function() {
+    stay();
+})
 
 
 createDeck();
 shuffle(deck);
 deal();
 console.log(deck);
-console.log(playerHand);
-console.log(dealerHand);
-  
-
-$('#play-button').on('click', () => {
-    console.log('Game started');
-})
-
-$('#hit-button').on('click', function hit() {
-    console.log('Hit me baby one more time');
-})
-
-$('#stay-button').on('click', function stay() {
-    console.log('Dont hit me anymore');
-})
+console.log('Player Hand', playerHand, getScore(playerHand));
+console.log('Dealer Hand', dealerHand, getScore(dealerHand));
